637ff12811ede107b73bc6995412e5e04010a3d4
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2024-06-27 17:59:30 -0300

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, HttpClientModule, RouterModule
+    CommonModule, HttpClientModule, RouterModule,
   ]
 })
 export class HomeModule { }
