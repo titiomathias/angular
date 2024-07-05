323a9782f28bc331961749732d247e0231576a7a
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2024-07-05 15:16:54 -0300

    content: content and buttons for missions

--- src/app/painel-secreto/painel-secreto.component.ts
+++ src/app/painel-secreto/painel-secreto.component.ts
@@ -14,21 +14,31 @@ export class PainelSecretoComponent {
   missions: MISSION[] = [
     {
       id: 1,
-      title: 'Missão 1',
-      description: 'Descrição da missão 1',
-      details: 'Detalhes da missão 1',
-      manager: 'Gerente 1',
-      level: 1,
+      title: 'Guerra de Gangues Prevista',
+      description: 'Guerra de Gangues na rua Jig Jig foi prevista na noite do dia 24/09.',
+      details: 'Os Tyger Claws pretendem iniciar uma guerra com as Mox pelo território da Rua Jig Jig. Como é previsível que danos colaterais ocorram no processo, foi solicitado a assistência de dois combatentes e dois médicos no local.',
+      manager: 'Gabriel',
+      level: 4,
       status: 0
     },
     {
       id: 2,
-      title: 'Missão 2',
-      description: 'Descrição da missão 2',
-      details: 'Detalhes da missão 2',
-      manager: 'Gerente 2',
-      level: 2,
+      title: 'Vítimas de guerra civil',
+      description: 'Guerra civil em Arroyo deixa moradores de rua e cidadãos locais extrmemante feridos.',
+      details: 'Na madrugada de hoje (05/06/2077 - 02:12) uma guerra civil entre os Lobos e a Militech na favela da rua 12 de Arroyo. Vários civis e moradores de rua foram atingidos no fogo cruzado. Solicitado assitência de três médicos e um combatente no local.',
+      manager: 'Mathias',
+      level: 3,
       status: 1
     }
   ];
+
+  selectedMissionId: number | null = null;
+
+  toggleDetails(missionId: number) {
+    if (this.selectedMissionId === missionId) {
+      this.selectedMissionId = null;
+    } else {
+      this.selectedMissionId = missionId;
+    }
+  }
 }
