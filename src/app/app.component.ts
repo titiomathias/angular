216eb04ea6f7c0f9133a178edc181ed1245f4d94
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2024-07-05 16:35:37 -0300

    feature: database server/service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/components/home.component';
 import { LoginComponent } from './login/components/login.component';
 import { FooterComponent } from './footer/components/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, LoginComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, FooterComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'greenhand'
+});
+
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+app.get('/data', (req, res) => {
+  const sql = 'SELECT * FROM users';
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
